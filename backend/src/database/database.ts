a2688caa01462bfbd1ad2da6bdf722ac6976a4a6
camiloduquee <camiloduquee@gmail.com>
2024-06-14 05:35:08 -0400

    update pg and config BD

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,4 +1,17 @@
 import { Sequelize } from 'sequelize'
-import { DATABASE_URL } from '../utils/config'
+import { DATABASE_URL, DATABASE_CONFIG } from '../utils/config'
+// import { PostgresDialect } from '@sequelize/postgres';
 
-export const sequelize = new Sequelize(DATABASE_URL as string)
+export const sequelize = new Sequelize(
+    DATABASE_CONFIG.database,
+    DATABASE_CONFIG.user,
+    DATABASE_CONFIG.password,
+    {
+      host: DATABASE_CONFIG.host,
+      port: Number(DATABASE_CONFIG.port),
+      dialect: DATABASE_CONFIG.dialect,
+      
+    }
+  );
+
+
--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config'
+import pg from "pg"
+import type { DatabaseConfig } from './types'
 
 export const PORT = +(process.env.PORT ?? 3001)
 export const DATABASE_URL = process.env.DATABASE_URL as string
@@ -6,6 +8,16 @@ export const Secret = process.env.JWT_SECRET as string
 export const Expire = process.env.JWT_EXPIRE as string
 export const Key = process.env.API_KEY as string
 export const hostFront = process.env.HOST_FRONT as string
+
+export const DATABASE_CONFIG: DatabaseConfig = {
+    database: process.env.DATABASE || '',
+    user: process.env.USER_BD || '',
+    password: process.env.PASSWORD || '',
+    host: process.env.HOST_BD || '',
+    port: process.env.PORT_BD || '',
+    dialect: 'postgres',
+    dialectModule: pg,
+}
 
 export const corsOptions = { origin: hostFront, credentials: true };
 
