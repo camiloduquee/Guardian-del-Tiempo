bd132fdc3a18a7b8c635584ee18c17d170df427c
camiloduquee <camiloduquee@gmail.com>
2024-06-15 01:11:38 -0400

    update

--- backend/src/docs/swagger.ts
+++ backend/src/docs/swagger.ts
@@ -8,7 +8,7 @@ import { type Router } from 'express'
 
 
 const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+  "https://cdn.jsdelivr.net/npm/swagger-ui-express@5.0.1/index.min.js";
 
 const filePath = path.resolve(__dirname, '../docs/swagger.yaml')
 
@@ -21,39 +21,40 @@ console.log(filePath)
 const file = fs.readFileSync(filePath, 'utf8')
 
 export const rutaFile = () => { return filePath }
-// const swaggerYaml = YAML.parse(file)
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-      termsOfService: "http://example.com/terms/",
-      contact: {
-        name: "API Support",
-        url: "http://www.exmaple.com/support",
-        email: "support@example.com",
-      },
-    },
-    servers: [
-      {
-        url: "https://guardiandeltiempo-server.vercel.app/",
-        description: "My API Documentation",
-      },
-    ],
-  },
-  // This is to call all the file
-  apis: ["src/**/*.js"],
-};
 
-const swaggerYaml = swaggerJsDoc(options);
+// const swaggerYaml = YAML.parse(file)
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Library API",
+//       version: "1.0.0",
+//       description: "A simple Express Library API",
+//       termsOfService: "http://example.com/terms/",
+//       contact: {
+//         name: "API Support",
+//         url: "http://www.exmaple.com/support",
+//         email: "support@example.com",
+//       },
+//     },
+//     servers: [
+//       {
+//         url: "https://guardiandeltiempo-server.vercel.app/",
+//         description: "My API Documentation",
+//       },
+//     ],
+//   },
+//   // This is to call all the file
+//   apis: ["src/**/*.js"],
+// };
+
+const swaggerYaml = YAML.parse(file);
 
 export const swaggerDocs = (app: Router) => {
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerYaml, { customCss: CSS_URL }))
   app.get('/api/docs.json', (_req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json')
-    res.send(swaggerYaml,{customCss: CSS_URL })
+    res.send(swaggerYaml)
   })
 
   console.log(`📄 Docs available at https://guardiandeltiempo-server.vercel.app/api/docs`)
