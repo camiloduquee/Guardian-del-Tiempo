99dc5322f0d287facfa5c12b22150b0cc83353ef
camiloduquee <camiloduquee@gmail.com>
2024-06-14 23:57:34 -0400

    update:cors off

--- backend/src/docs/swagger.ts
+++ backend/src/docs/swagger.ts
@@ -19,7 +19,7 @@ if (!fs.existsSync(filePath)) {
 const file = fs.readFileSync(filePath, 'utf8')
 const swaggerYaml = YAML.parse(file)
 
-export const swaggerDocs = (app: Router, port: number) => {
+export const swaggerDocs = (app: Router ) => {
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerYaml, {customCss:CSS_URL}))
   app.get('/api/docs.json', (_req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json')
