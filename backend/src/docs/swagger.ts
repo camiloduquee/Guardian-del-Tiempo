1d3253664019b5a66a7a28642749ed246df43c57
camiloduquee <camiloduquee@gmail.com>
2024-06-15 02:27:22 -0400

    add: custom css fix

--- backend/src/docs/swagger.ts
+++ backend/src/docs/swagger.ts
@@ -22,7 +22,11 @@ const file = fs.readFileSync(filePath, 'utf8')
 const swaggerYaml = YAML.parse(file);
 
 export const swaggerDocs = (app: Router) => {
-  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerYaml, { customCssUrl: CSS_URL }))
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerYaml, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+  }))
   app.get('/api/docs.json', (_req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json')
     res.send(swaggerYaml)
